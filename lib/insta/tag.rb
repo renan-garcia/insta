9f0bf1ff82d59f87028a74adcb1f98e7bbb75c21
Renan Garcia <email@renangarcia.me>
2018-07-12 16:01:10 -0300

    Update find tag url

--- lib/insta/tag.rb
+++ lib/insta/tag.rb
@@ -1,7 +1,7 @@
 module Insta
   module Tag
     def self.find(tag, data = {})
-      endpoint = %Q[https://www.instagram.com/graphql/query/?query_id=17875800862117404&variables={"tag_name":"#{tag}","first":42,"after":"#{data.dig(:end_cursor)}"}]
+      endpoint = %[https://www.instagram.com/graphql/query/?query_hash=ded47faa9a1aaded10161a2ff32abb6b&variables={"tag_name":"#{tag}","first":42,"after":"#{data.dig(:end_cursor)}"}]
       proxies = Insta::ProxyManager.new data[:proxies] unless data[:proxies].nil?
       result = Insta::API.http(
         url: endpoint,
